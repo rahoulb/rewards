796513eb47a01b40e5ffd45d6d3b3dad1210eb35
Rahoul Baruah <rahoul@3hv.co.uk>
2015-02-09 15:31:01 +0000

    Added some docs

--- lib/Rewards/service.rb
+++ lib/Rewards/service.rb
@@ -1,6 +1,12 @@
 require_relative './invalid_account_number'
 
 module Rewards
+  # This service uses an Eligibility Service to check if the given account number is eligible for some rewards
+  # It is expected that the eligibility service responds to:
+  #     check_eligibility_for(account_number) 
+  # returning an array of channels for which the account can claim rewards on.
+  # It may raise an InvalidAccountNumber exception if the eligibility service reports the account number back as invalid
+  # Otherwise it should always return an array of rewards
   class Service < Struct.new(:eligibility_service)
 
     def rewards_for account_number: nil, subscriptions: []
